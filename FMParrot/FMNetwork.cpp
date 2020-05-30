3acc7a196b0e57a7a5436c5f53be93df53370c01
Geoffrey Merck <F4FXL@users.noreply.github.com>
2020-05-30 07:28:49 +0200

    Handle Poll message correctly

--- FMParrot/FMNetwork.cpp
+++ FMParrot/FMNetwork.cpp
@@ -61,7 +61,7 @@ unsigned int CFMNetwork::read(unsigned char* data, unsigned int len)
 	m_address.s_addr = address.s_addr;
 	m_port = port;
 
-	if (::memcmp(data, "FMP", 3U) == 0 && length == 17) {			// A poll
+	if (::memcmp(data, "FMP", 3U) == 0) {			// A poll
 		write(data, length);
 		return 0U;
 	} else if (len >=  3 && (::memcmp(data, "FMD", 3U) || ::memcmp(data, "FME", 3U))) {
